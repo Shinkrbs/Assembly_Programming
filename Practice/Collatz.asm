5ac36886e76dc7daeaae985f5048408826527700
Shn Kerby Dolamos <kerbydolamoscs@gmail.com>
2025-03-08 23:42:44 +0800

    [Practice] Added new practice Collatz Conjecture

--- Practice/Collatz.asm
+++ Practice/Collatz.asm
@@ -1,5 +1,48 @@
+.model small 
+.stack 64
+ 
+.data 
+   ; Goal Count Number of steps to reach collatz sequence.
+   input dw 10
+   result dw ?
+    
+.code 
 
+   main proc near 
+   mov AX,@data 
+   mov DS,AX 
+   mov ES,AX 
+   
+   mov CX, 0
+   mov AX, input
+   
+   loop:
+        cmp AX, 1
+        jz done
+        
+        test AX, 1
+        jz even
+        
+        jmp odd
 
-
-
-
+   odd: 
+        mov DX, 0
+        mov BX, 3
+        mul BX
+        add AX, 1
+        inc CX
+        jmp loop
+        
+   even:
+        mov DX, 0
+        mov BX, 2
+        div BX
+        inc CX 
+        jmp loop
+        
+   done:
+        mov result, CX
+        mov AX,4C00H 
+        int 21H 
+ 
+   main endp 
